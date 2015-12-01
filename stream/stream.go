cdf2bcc74709aeaf22d453ac10e2cf4027237752
Kris Brandow <kris.brandow@gmail.com>
2015-11-30 22:30:38 -0500

    Adding mode to the stream and adding Start method to the stream.

--- stream/stream.go
+++ stream/stream.go
@@ -1,26 +1,107 @@
 package stream
 
 import (
+	"crypto/rand"
 	"encoding/xml"
 	"errors"
+	"fmt"
 	"io"
 
 	"github.com/skriptble/nine/element"
 )
 
+// Mode determines the mode of the stream.
+//
+// Currently this is either Initiating or Receiving for the stream initiating
+// entity or receiving entity, respectively.
+type Mode int
+
+const (
+	Initiating Mode = iota
+	Receiving
+)
+
 // ErrStreamClosed is the error returned when the stream has been closed.
 var ErrStreamClosed = errors.New("Stream Closed")
 
+// ErrHeaderNotSet is the error returned when start has been called on a stream
+// in initiating mode and the header has not yet been set.
+var ErrHeaderNotSet = errors.New("Stream Header has not been set")
+
 // Stream represents an XML Stream
 type Stream struct {
+	Header
+
 	*xml.Decoder
-	rw io.ReadWriter
+	rw   io.ReadWriter
+	mode Mode
 }
 
-// New creates a new Stream from the given io.ReadWriter
-func New(rw io.ReadWriter) Stream {
+// New creates a new Stream from the given io.ReadWriter.
+//
+// The mode parameter determines if this the initiating or recieving entity.
+func New(rw io.ReadWriter, mode Mode) Stream {
 	dec := xml.NewDecoder(rw)
-	return Stream{Decoder: dec, rw: rw}
+	return Stream{Decoder: dec, rw: rw, mode: mode}
+}
+
+// Start writes the stream header to the stream.
+//
+// If the stream is in initiating mode, the Header must be set before this
+// method is called.
+//
+// If the server is in receiving mode and the Header has not been set this
+// method will read a token from the stream, if the token is a stream header
+// it will generate a new stream id, flip the to and from fields, and set the
+// Header field.
+//
+// If the conditions are met for either mode, the stream header is sent and any
+// errors are returned.
+//
+// TODO(skriptble): Should there be a default from for the recieving mode?
+// Or should an error be returned if there is no from after the flip?
+func (s Stream) Start() (err error) {
+	if s.mode == Initiating {
+		if s.Header == (Header{}) {
+			return ErrHeaderNotSet
+		}
+		_, err = s.writeHeader()
+		return
+	}
+
+	if s.Header == (Header{}) {
+		var elem element.Element
+		var hdr Header
+		var id string
+
+		elem, err = s.Next()
+		if err != nil {
+			return
+		}
+
+		hdr, err = NewHeader(elem)
+		if err != nil {
+			return
+		}
+
+		id, err = genStreamID()
+		if err != nil {
+			return
+		}
+
+		hdr.ID = id
+		hdr.To, hdr.From = hdr.From, hdr.To
+		s.Header = hdr
+	}
+
+	_, err = s.writeHeader()
+	return
+}
+
+// TODO(skriptble): Does this need to return the bytes sent?
+func (s Stream) writeHeader() (n int, err error) {
+	b := s.Header.WriteBytes()
+	return s.Write(b)
 }
 
 // Write allows the writting of arbitrary bytes to the stream.
@@ -110,4 +191,17 @@ func (s Stream) childElements() (children []element.Token, err error) {
 			children = append(children, element.CharData{Data: data})
 		}
 	}
+}
+
+func genStreamID() (string, error) {
+	id := make([]byte, 16)
+	_, err := rand.Read(id)
+	if err != nil {
+		return "", err
+	}
+
+	id[8] = (id[8] | 0x80) & 0xBF
+	id[6] = (id[6] | 0x40) & 0x4F
+
+	return fmt.Sprintf("%x", id), nil
 }
